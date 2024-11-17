fc77216d021ceb6c5fec83a276bba4be372e8e47
Tommy840602 <84989346+Tommy840602@users.noreply.github.com>
2024-11-17 13:27:11 +0800

    frist commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from typing import Union
+from fastapi import FastAPI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+key = os.getenv("FAKE_VALUE")
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
